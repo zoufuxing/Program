4130a7073f70f73a21f9d9fe3445eac7ba446f4b
zoufuxing <zoufuxing@gmail.com>
2013-02-20 15:16:32 +0800

    123.pl

---
+++ Perl/123.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl 
+#===============================================================================
+#
+#         FILE:  123.pl
+#
+#        USAGE:  ./123.pl  
+#
+#  DESCRIPTION:  
+#
+#      OPTIONS:  ---
+# REQUIREMENTS:  ---
+#         BUGS:  ---
+#        NOTES:  ---
+#       AUTHOR:  YOUR NAME (), 
+#      COMPANY:  
+#      VERSION:  1.0
+#      CREATED:  2013/02/20 15时16分17秒
+#     REVISION:  ---
+#===============================================================================
+
+use strict;
+use warnings;
+
+
+
